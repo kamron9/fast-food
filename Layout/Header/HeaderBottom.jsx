c326053fcc60bd879d69e84bc7b3bba01ce5d261
kamron9 <kamronalimov80@gmail.com>
2024-01-16 20:43:00 +0500

    header navigations created

---
+++ Layout/Header/HeaderBottom.jsx
@@ -0,0 +1,13 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/pagination";
+
+// import required modules
+import { Pagination, Autoplay } from "swiper/modules";
+
+const HeaderBottom = () => {
+  return <div></div>;
+};
+
+export default HeaderBottom;
